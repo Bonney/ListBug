95794d5374f46c4d7dffb8627140757b0cdf5659
Jason Ji <uberjason@gmail.com>
2020-02-25 17:34:25 -0500

    Add README

--- ListBug/ContentView.swift
+++ ListBug/ContentView.swift
@@ -9,12 +9,48 @@
 import SwiftUI
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        NavigationView {
+            List {
+                Section(header: Text("One")) {
+                    ForEach(0..<5) { i in
+                        NavigationLink(destination: DetailView(value: i)) {
+                            Text("Thing \(i)")
+                        }.frame(minWidth: 200, idealHeight: 30, alignment: .leading)
+                    }
+                }
+                Section(header: Text("Two")) {
+                    ForEach(6..<10) { j in
+                        NavigationLink(destination: DetailView(value: j)) {
+                            Text("Item \(j)")
+                        }.frame(minWidth: 200, idealHeight: 30, alignment: .leading)
+                    }
+                }
+            }.frame(maxWidth: .infinity, maxHeight: .infinity)
+        }.listStyle(myListStyle())
+    }
+
+    func myListStyle() -> some ListStyle {
+        #if os(macOS)
+        return SidebarListStyle()       // Incorrectly duplicates items in the first section.
+        #else
+        return DefaultListStyle()       // Correctly displays items in both sections on macOS and iOS.
+        #endif
+
     }
 }
 
+struct DetailView: View {
+    var value: Int
+
+    var body: some View {
+        VStack {
+            Text("Selected \(value)").frame(maxWidth: .infinity, maxHeight: .infinity)
+
+        }
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ README.md
@@ -0,0 +1,6 @@
+# ListBug
+
+SidebarListStyle on macOS mixes up items in the sidebar list. DefaultListStyle behaves correctly on macOS and iOS.
+
+Run the project. The first table section should have items "Thing 0", "Thing 1"...up to "Thing 4". The second section should have items "Item 6", "Item 7",... up to "Item 9".
+
