97853200243c318bb2463810c0a34d10e2ffe414
Jason Ji <jason.ji@capitalone.com>
2020-02-25 15:39:43 -0500

    Initial Commit

---
+++ ListBug/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ListBug
+//
+//  Created by Jason Ji on 2/25/20.
+//  Copyright © 2020 Jason Ji. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
